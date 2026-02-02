41315d852d46656ab625e77716c191dc18457dc7
FreedomEagle217 <jacobochristopher743@gmail.com>
2026-02-02 16:37:09 -0700

    Created Controller with home and listProducts methods

--- src/main/java/com/coffeshop/menu/CoffeshopMenuApplication.java
+++ src/main/java/com/coffeshop/menu/CoffeshopMenuApplication.java
@@ -1,5 +1,6 @@
 package com.coffeshop.menu;
 
+import com.coffeshop.menu.controller.ProductController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
---
+++ src/main/java/com/coffeshop/menu/controller/ProductController.java
@@ -0,0 +1,32 @@
+package com.coffeshop.menu.controller;
+
+import com.coffeshop.menu.model.Product;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/products")
+public class ProductController {
+    private List<Product> productList = List.of(
+            new Product(1, "Cappucino", 7.50f),
+            new Product(2, "Macha", 8.00f),
+            new Product(3, "Frappucino", 8.50f));
+    @RequestMapping("/")
+    @ResponseBody
+    public String home(){
+        return "Welcome to the Coffee Shop!";
+    }
+
+    public String listProducts(){
+        String productDisplay = "<strong> Product list </strong><hr>";
+        for (Product p : productList){
+            String productInfo = " " + p.getId() + " " + p.getProductName() +
+                                " " + p.getPrice() + "\n";
+            productDisplay += productInfo;
+        }
+        return productDisplay;
+    }
+}
