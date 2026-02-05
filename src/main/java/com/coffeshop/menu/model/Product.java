c0bd910775c5887e14cc179daba24d9ea06c221d
FreedomEagle217 <jacobochristopher743@gmail.com>
2026-02-05 13:05:13 -0700

    Transitioned Product.java into a JPA entity

--- src/main/java/com/coffeshop/menu/model/Product.java
+++ src/main/java/com/coffeshop/menu/model/Product.java
@@ -1,8 +1,23 @@
 package com.coffeshop.menu.model;
 
+//import jakarta.validation.constraints.NotNull;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="Products")
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    //@NotNull(message="A product name is required.")
+    //@Size(min=2, max=50, message="Product name must be between 2 and 50 characters.")
     private String productName;
+    //@NotNull(message="A product price is required.")
+    //@Min(value=1,message="Product price must be a positive value.")
     private float price;
 
     public Product(int id, String productName, float price) {
