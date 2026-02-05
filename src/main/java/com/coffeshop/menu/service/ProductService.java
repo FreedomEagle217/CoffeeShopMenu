65a9b582d6f6440cc664ae013d8999d977772d2a
FreedomEagle217 <jacobochristopher743@gmail.com>
2026-02-05 13:14:15 -0700

    Added service and implementation

---
+++ src/main/java/com/coffeshop/menu/service/ProductService.java
@@ -0,0 +1,10 @@
+package com.coffeshop.menu.service;
+
+import java.util.List;
+
+import com.coffeshop.menu.model.Product;
+
+public interface ProductService {
+    List<Product> getAllProducts();
+    void saveProduct(Product product);
+}
---
+++ src/main/java/com/coffeshop/menu/service/ProductServiceImplement.java
@@ -0,0 +1,24 @@
+package com.coffeshop.menu.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.coffeshop.menu.repository.ProductRepository;
+import com.coffeshop.menu.model.Product;
+import java.util.List;
+
+@Service
+public class ProductServiceImplement implements ProductService {
+    @Autowired
+    private ProductRepository productRepository;
+    @Override
+    public List<Product> getAllProducts(){
+        return productRepository.findAll();
+    }
+
+    @Override
+    public void saveProduct(Product product){
+        this.productRepository.save(product);
+    }
+
+}
